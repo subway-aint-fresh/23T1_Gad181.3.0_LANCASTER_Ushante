d7f8424b26e6ace32f70deadc4b2f8bd0554d2b4
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-04-12 02:03:40 +1000

    Adding lose screen
    - added lose screen
    - lose removes players
    - win removes players

--- Assets/Scripts/EndFlag.cs
+++ Assets/Scripts/EndFlag.cs
@@ -8,6 +8,33 @@ public class EndFlag : MonoBehaviour
     public GameObject player2;
 
     public GameObject winScreen;
+    public GameObject p1;
+    public GameObject p2;
 
+    private bool player1Triggered = false;
+    private bool player2Triggered = false;
 
+    // This method is called when a player enters the trigger area
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player1)
+        {
+            Debug.Log("area entered by p1");
+            player1Triggered = true;
+        }
+        else if (other.gameObject == player2)
+        {
+            Debug.Log("area entered by p2");
+            player2Triggered = true;
+        }
+
+        // If both players have triggered the win condition, show the win screen
+        if (player1Triggered && player2Triggered)
+        {
+            p1.SetActive(false);
+            p2.SetActive(false);
+            winScreen.SetActive(true);
+            
+        }
+    }
 }
--- Assets/Scripts/Poison.cs
+++ Assets/Scripts/Poison.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class Poison : MonoBehaviour
-{ 
+{
+    public GameObject endScreen; //end screen pop up
+    public GameObject p1;
+    public GameObject p2;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             //endgame, throw up end screen
             Debug.Log("End game");
+
+            p1.SetActive(false);
+            p2.SetActive(false);
+            endScreen.SetActive(true);
+
         }
     }
 }
