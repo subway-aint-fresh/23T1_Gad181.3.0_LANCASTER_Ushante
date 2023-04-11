940121938dbc0b106703e34e8b997084f82d4e16
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-04-11 23:25:23 +1000

    Setting up second hazard
    - added button and elevator assets

--- Assets/Scripts/Poison.cs
+++ Assets/Scripts/Poison.cs
@@ -1,26 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class Poison : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+{ 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            //endgame
+            //endgame, throw up end screen
             Debug.Log("End game");
         }
     }
