b22c0dfbcc3808544e7d7613e56f5e8b262ff4fb
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-04-11 21:08:29 +1000

    changes to movement script
    renamed script and removed p2 script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class P1Controller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
