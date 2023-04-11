7f43eb6af2bc06212f8d8468854ad26c48af64ef
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-04-11 19:58:59 +1000

    Adding jump to character control

--- Assets/Scripts/MoveArrows.cs
+++ Assets/Scripts/MoveArrows.cs
@@ -9,12 +9,14 @@ public class MoveArrows : MonoBehaviour
     float MovementX;
     float MovementY;
 
-    Rigidbody2D Rb;
+    private Rigidbody2D Rb;
+    private Rigidbody2D Rb2;
 
     // Start is called before the first frame update
     void Start()
     {
         Rb = GetComponent<Rigidbody2D>();
+        Rb2 = transform.GetComponent<Rigidbody2D>();
         MovementX = 0;
         MovementY = 0;
     }
@@ -23,28 +25,26 @@ public class MoveArrows : MonoBehaviour
     void Update()
     {
       Rb.velocity = new Vector2 ( MovementX * Speed * Time.deltaTime, MovementY * Speed * Time.deltaTime );
-        
-      if (Input.GetKeyDown( KeyCode.UpArrow ) ) 
+      
+         //jump
+        if (Input.GetKeyDown( KeyCode.UpArrow ) ) 
         {
-            MovementY = 1;
+            float jumpVelocity = 60f;
+            Rb2.velocity = Vector2.up * jumpVelocity;
         }
 
-      if (Input.GetKeyDown( KeyCode.LeftArrow ) )
+        if (Input.GetKeyDown( KeyCode.LeftArrow ) )
         {
             MovementX = -1;
         }
 
-      if (Input .GetKeyDown( KeyCode.RightArrow ) )
+        if (Input .GetKeyDown( KeyCode.RightArrow ) )
         {
             MovementX = 1;
         }
 
 
       //makes sure that the controller doesnt continue to move after the buttons finished being pushed
-      if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) )
-        {
-            MovementY = 0;
-        }
 
         if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
         {
