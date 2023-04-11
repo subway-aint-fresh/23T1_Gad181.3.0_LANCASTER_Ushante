926fbe1e5f49ac729f175d03ded76dbe0514f6cf
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-04-12 01:45:12 +1000

    win condition script
    added script for win condition on flag
    
    added box collider

---
+++ Assets/Scripts/EndFlag.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndFlag : MonoBehaviour
+{ 
+    public GameObject player1;
+    public GameObject player2;
+
+    public GameObject winScreen;
+
+
+}
